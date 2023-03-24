9f5adfd910307db1d0b9f0375721daa6dc6457a5
Jose264842 <josephaj.28822@gmail.com>
2023-03-24 17:50:29 +0530

    4th Commit

--- eclipse-workspaceJose/Cicd/src/test/java/Training/Baseclass.java
+++ eclipse-workspaceJose/Cicd/src/test/java/Training/Baseclass.java
@@ -1,18 +1,20 @@
 package Training;
 
+import java.io.IOException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 			
 public class Baseclass 
 {
 	public static WebDriver driver;
     
-   
-	public static void main(String[] args) throws Throwable 
+   @Test
+	public void invoke() throws IOException, InterruptedException 
 	{
-		
-	
 				System.setProperty("webdriver.chrome.driver", "D:\\Software\\chromedriver_win32\\chromedriver.exe");
 				WebDriverManager.chromedriver().setup();
 				Thread.sleep(5000);
@@ -20,9 +22,7 @@ public class Baseclass
 				System.out.println("Browser is launched !");
 				driver.manage().window().maximize();
 				driver.get("https://www.google.com/");
-				//driver.get("https://admin:Test@123@qaenv.bollybet.net");
-				
-	
+				driver.get("https://admin:Test@123@qaenv.bollybet.net");	
      }
        
 }
